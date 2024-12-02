c6a385fdf7b3313b7ba1bb396ea4f3e6d1b94021
aeciojose <aeciojunior45@gmail.com>
2024-12-02 17:58:09 -0300

    aprendendo_consolelog_e_window

---
+++ módulo3_javascript/exercicio01/exercicio01.js
@@ -0,0 +1,25 @@
+const primeiroNome = prompt("Digite o nome: ")
+const sobrenome = prompt("Digite o sobrenome: ")
+const estudo = prompt("Digite o campo de estudo: ")
+const nasc = prompt("Digite o ano de nascimento")
+
+alert(
+    "Recruta cadastrado com sucesso! \n" + 
+    "Nome completo: " + primeiroNome + " "+ sobrenome + "Campo de estudo: " + estudo + 
+    "Ano de nascimento: " + (2024-nasc)
+
+)
+
+
+
+
+
+
+
+//pt 2
+// var numberOne = parseFloat(prompt("Digite o primeiro valor: "))
+// var numberTwo = parseFloat(prompt("Digite o segundo valor: "))
+// alert(numberOne+numberTwo)
+// alert(numberOne * numberTwo) 
+// alert(numberOne/numberTwo)
+// alert(numberOne- numberTwo)
--- módulo3_javascript/recursos_nativos/index.js
+++ módulo3_javascript/recursos_nativos/index.js
@@ -1,1 +1,14 @@
-console.log()
+let nome = "aecio"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+//nome = window.prompt("insira seu nome: ")
+let confirmacao = window.confirm("Confirma essa ação? ")
+console.log(confirmacao)
+
+//window é um objeto global, sempre está disponível no navegador
+
+//faz referência a nossa página html
+// --> document
